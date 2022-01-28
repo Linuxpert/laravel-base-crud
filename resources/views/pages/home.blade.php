69e069980c0e8473212cd9aba4069c9a35293388
Linuxpert <g.manfre10@gmail.com>
2022-01-28 16:44:32 +0100

    added css

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,11 +7,12 @@
     @foreach ($comics as $comic)
         <ul>
             <b>
+                Fumetto:
                 <a href="{{ route('show', $comic -> id) }}">
                     {{$comic -> title}}
                 </a>
 
-                - {{$comic -> author}}
+                - autore:{{$comic -> author}}
             </b>
         </ul>
     @endforeach
